1c94124cb2774d99d0ed8682165af2dfb687e7f2
Seva D. <43249611+vaguue@users.noreply.github.com>
2024-12-12 17:20:05 +0300

    Create gpu.sh

---
+++ scripts/gpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
+apt-get update
+apt-get install cuda
+
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey |  apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+apt-get update
+apt-get install nvidia-container-runtime
+
+add-apt-repository ppa:graphics-drivers/ppa
+apt update
+ubuntu-drivers autoinstall
